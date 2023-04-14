ce6c9b4409437007f29bbebda0537a36c5218ceb
Kira Selby <kaselby@uwaterloo.ca>
2023-04-14 12:06:44 -0400

    launch

---
+++ my_scripts/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=fsdm
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=rtx6000,t4v2
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem=45GB
+#SBATCH --exclude=gpu109
+
+
+sh script/main.sh 4 vfsddpm_cifar100_vit_film_mean_sigma
