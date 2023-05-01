37f0b8ca7d8255b010e50d0912d5541222db7f4e
Kira Selby <kaselby@uwaterloo.ca>
2023-05-01 10:53:07 -0400

    fix sample size issue??

---
+++ my_scripts/eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=fsdm
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=a40
+#SBATCH --cpus-per-gpu=4
+#SBATCH --mem=45GB
+#SBATCH --exclude=gpu109
+
+
+sh script/sample_conditional.sh 0 vfsddpm_${1}_vit_lag_meanpatch_sigma
