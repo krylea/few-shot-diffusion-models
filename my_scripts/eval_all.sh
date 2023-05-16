501d6ee8bc82487b0561f7e17b5f39e5bf06fb52
Kira Selby <kaselby@uwaterloo.ca>
2023-05-16 16:55:42 -0400

    sample eval

--- my_scripts/eval_all.sh
+++ my_scripts/eval_all.sh
@@ -4,10 +4,11 @@ NREF=(1 3 10 30)
 
 dataset=$1
 image_size=${2:-128}
-reset=${3:-0}
+sample_eval=${3:-0}
+reset=${4:-0}
 
 
 for nref in "${NREF[@]}"
 do
-    sbatch my_scripts/eval2.sh $dataset $nref $image_size $reset
+    sbatch my_scripts/eval2.sh $dataset $nref $image_size $sample_eval $reset
 done
