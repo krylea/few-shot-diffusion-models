c1dabb220fc60343d585ac1093470894c619feb9
Kira Selby <kaselby@uwaterloo.ca>
2023-05-16 11:56:39 -0400

    fix fucking eval

--- my_scripts/eval_all.sh
+++ my_scripts/eval_all.sh
@@ -3,8 +3,9 @@
 NREF=(1 3 10 30)
 
 dataset=$1
+reset=${2:-0}
 
 for nref in "${NREF[@]}"
 do
-    sbatch my_scripts/eval2.sh $dataset $nref
+    sbatch my_scripts/eval2.sh $dataset $nref $reset
 done
